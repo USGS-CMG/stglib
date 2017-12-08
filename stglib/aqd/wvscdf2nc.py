cb25b5513ee37e186b023df4fe477db6ad059783
Dan Nowacki <dnowacki@usgs.gov>
2017-12-08 16:09:23 -0500

    VEL->ds

--- stglib/aqd/wvscdf2nc.py
+++ stglib/aqd/wvscdf2nc.py
@@ -7,31 +7,31 @@ from . import qaqc
 def cdf_to_nc(cdf_filename, atmpres=False):
 
     # Load raw .cdf data
-    VEL = qaqc.load_cdf(cdf_filename, atmpres=atmpres)
+    ds = qaqc.load_cdf(cdf_filename, atmpres=atmpres)
 
     # Clip data to in/out water times or via good_ens
-    VEL = utils.clip_ds(VEL)
+    ds = utils.clip_ds(ds)
 
     # Create water_depth variables
-    VEL = utils.create_water_depth(VEL)
+    ds = utils.create_water_depth(ds)
 
     # Create depth variable depending on orientation
-    VEL, T = qaqc.set_orientation(VEL, VEL['TransMatrix'].values)
+    ds, T = qaqc.set_orientation(ds, ds['TransMatrix'].values)
 
-    VEL = qaqc.make_bin_depth(VEL)
+    ds = qaqc.make_bin_depth(ds)
 
-    VEL = qaqc.ds_rename(VEL, waves=True)
+    ds = qaqc.ds_rename(ds, waves=True)
 
-    VEL = qaqc.ds_add_attrs(VEL, waves=True)
+    ds = qaqc.ds_add_attrs(ds, waves=True)
 
-    VEL = utils.add_min_max(VEL)
+    ds = utils.add_min_max(ds)
 
-    nc_filename = VEL.attrs['filename'] + 'wvsb-cal.nc'
+    nc_filename = ds.attrs['filename'] + 'wvsb-cal.nc'
 
-    VEL.to_netcdf(nc_filename, unlimited_dims='time')
+    ds.to_netcdf(nc_filename, unlimited_dims='time')
     print('Done writing netCDF file', nc_filename)
 
     # rename time variables after the fact to conform with EPIC/CMG standards
     utils.rename_time(nc_filename)
 
-    return VEL
+    return ds
