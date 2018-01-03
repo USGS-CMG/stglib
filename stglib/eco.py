d939eb8e2a1f52445ce87c30f55c1aec9e476794
Dan Nowacki <dnowacki@usgs.gov>
2018-01-03 18:11:00 -0500

    Support loading PAR sensors.

--- stglib/eco.py
+++ stglib/eco.py
@@ -1,55 +1,57 @@
-#!/usr/bin/env python
-
 from __future__ import division, print_function
 import pandas as pd
 import xarray as xr
 
+
+def read_par(filnam, spb=False, skiprows=None, skipfooter=0):
+
+    names = ['date', 'time', 'counts']
+
+    par = read_eco_csv(filnam, names, skiprows=skiprows, skipfooter=skipfooter)
+
+    par = eco_pd_to_xr(par, spb=spb)
+
+    return par
+
+
 def read_ntu(filnam, spb=False, skiprows=None, skipfooter=0):
-    ntu = pd.read_csv(filnam,
+
+    names = ['date', 'time', 'a', 'counts', 'b']
+
+    ntu = read_eco_csv(filnam, names, skiprows=skiprows, skipfooter=skipfooter)
+
+    ntu = eco_pd_to_xr(ntu, spb=spb)
+
+    return ntu
+
+
+def read_eco_csv(filnam, names, skiprows=None, skipfooter=0):
+
+    return pd.read_csv(filnam,
                       sep='\t',
-                      names=['date', 'time', 'a', 'counts', 'b'],
+                      names=names,
                       parse_dates=[['date', 'time']],
                       infer_datetime_format=True,
                       engine='python',
                       skiprows=skiprows,
                       skipfooter=skipfooter)
 
+
+def eco_pd_to_xr(df, spb=False):
+
     if spb:
-        times = ntu['date_time'].values.reshape((-1, spb))[:, int(spb/2)] # get middle time
-        counts = ntu['counts'].values.reshape((-1, spb))
-        a = ntu['a'].values.reshape((-1, spb))
-        b = ntu['b'].values.reshape((-1, spb))
+        times = df['date_time'].values.reshape((-1, spb))[:, int(spb/2)] # get middle time
+        counts = df['counts'].values.reshape((-1, spb))
         sample = range(spb)
 
         ds = xr.Dataset({'time': ('time', times),
                          'counts': (['time', 'sample'], counts),
-                         'a': (['time', 'sample'], a),
-                         'b': (['time', 'sample'], b),
                          'sample': ('sample', sample)})
     else:
-        times = ntu['date_time']
-        counts = ntu['counts']
+        times = df['date_time']
+        counts = df['counts']
 
         ds = xr.Dataset({'time': ('time', times),
                          'counts': ('time', counts)})
 
     return ds
-
-# TODO: implement read_par()
-
-def main():
-    import argparse
-    parser = argparse.ArgumentParser(description='Convert raw NTU tab-delimited text files to netCDF')
-    parser.add_argument('ntuname', help='raw NTU filename')
-    parser.add_argument('--spb', help='samples per burst, default 10', default=10)
-
-    args = parser.parse_args()
-
-    ntu = read_ntu(args.ntuname)
-
-    ntu = pd_to_xr(ntu, spb=args.spb)
-
-    print(ntu)
-
-if __name__ == '__main__':
-    main()
