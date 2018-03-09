a00a2c02d50a6d9724dd11c89a8dcf4473175cc6
Dan Nowacki <dnowacki@usgs.gov>
2018-03-09 17:34:36 -0500

    EXO run scripts

---
+++ scripts/runexocdf2nc.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import sys
+sys.path.insert(0, '/Users/dnowacki/Documents/stglib')
+import stglib
+import argparse
+
+parser = argparse.ArgumentParser(description='Convert raw EXO .cdf format to processed .nc files')
+parser.add_argument('cdfname', help='raw .CDF filename')
+parser.add_argument('--atmpres', help='path to cdf file containing atmopsheric pressure data')
+
+args = parser.parse_args()
+
+if args.atmpres:
+    ds = stglib.exo.cdf_to_nc(args.cdfname, atmpres=args.atmpres)
+else:
+    ds = stglib.exo.cdf_to_nc(args.cdfname)
