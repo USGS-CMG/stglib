4a02df2dea7d823da6aece9f4014f415646a6399
Dan Nowacki <dnowacki@usgs.gov>
2018-05-01 16:29:41 -0400

    Add new homebrew waves code for AQD waves

---
+++ stglib/aqd/wvsnc2waves.py
@@ -0,0 +1,67 @@
+from __future__ import division, print_function
+import xarray as xr
+import sys
+from ..core import utils
+
+
+def nc_to_waves(nc_filename):
+
+    ds = xr.open_dataset(nc_filename, autoclose=True, decode_times=False)
+
+    ds = utils.epic_to_cf_time(ds)
+
+    ds = utils.create_epic_time(ds)
+
+    spec = make_waves_ds(ds)
+
+    for k in ['wp_peak', 'wh_4061', 'wp_4060', 'pspec']:
+        ds[k] = spec[k]
+
+    ds = utils.create_water_depth(ds)
+
+    # Remove old variables as we just want to keep the wave statistics
+    ds = ds.drop(['P_1',
+                  'P_1ac',
+                  'sample',
+                  'Tx_1211',
+                  'vel1_1277',
+                  'vel2_1278',
+                  'vel3_1279',
+                  'U',
+                  'V',
+                  'W',
+                  'avgamp1',
+                  'avgamp2',
+                  'avgamp3',
+                  'AGC1_1221',
+                  'AGC2_1222',
+                  'AGC3_1223',
+                  'TransMatrix',
+                  'nrecs',
+                  'burst',
+                  'soundspeed',
+                  'Battery',
+                  'Hdg_1215',
+                  'Ptch_1216',
+                  'Roll_1217'])
+
+    ds = utils.trim_max_wp(ds)
+
+    ds = utils.trim_min_wh(ds)
+
+    ds = utils.trim_max_wh(ds)
+
+    ds = utils.trim_wp_ratio(ds)
+
+    # Add attrs
+    ds = utils.ds_add_attrs(ds)
+
+    nc_filename = ds.attrs['filename'] + 'wvs-a.nc'
+
+    ds = utils.rename_time(ds)
+
+    ds.to_netcdf(nc_filename)
+
+    print('Done creating', nc_filename)
+
+    return ds
