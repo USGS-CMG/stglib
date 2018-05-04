7f985ee5ca7e39a52a9f576e0651a7bf2c5b086b
Dan Nowacki <dnowacki@usgs.gov>
2018-05-04 17:18:20 -0400

    Test coordinate transforms

--- stglib/tests/test_utils.py
+++ stglib/tests/test_utils.py
@@ -21,5 +21,63 @@ class TestTimes(unittest.TestCase):
         assert np.all(difftime <= np.timedelta64(1, 'ms'))
 
 
+class TestAqd(unittest.TestCase):
+
+    def setUp(self):
+        self.T = np.array([[2896, 2896, 0],
+                           [-2896, 2896, 0],
+                           [-2896, -2896, 5792]])/4096
+
+        self.vel1 = np.expand_dims([0.23, 0.23, 0.23], 1)
+        self.vel2 = np.expand_dims([-0.52, -0.52, -0.52], 1)
+        self.vel3 = np.expand_dims([0.12, 0.12, 0.12], 1)
+
+        self.h = np.expand_dims([0, 10, 230], 1)
+        self.p = np.expand_dims([0, -5, 5], 1)
+        self.r = np.expand_dims([0, 3, -3], 1)
+
+    def test_coord_transform(self):
+        # Using Nortek example m-file and compare to Matlab results
+        # http://www.nortekusa.com/lib/forum-attachments/coordinate-transformation/view
+
+        u, v, w = stglib.aqd.qaqc.coord_transform(self.vel1,
+                                                  self.vel2,
+                                                  self.vel3,
+                                                  self.h,
+                                                  self.p,
+                                                  self.r,
+                                                  self.T,
+                                                  'BEAM')
+
+        result = np.hstack((u, v, w))
+        expected = np.array([[0.530273437500000, -0.205039062500000, 0.374726562500000],
+                             [0.510589752632478, -0.266778740685713, 0.363012589777355],
+                             [-0.144471300248944, 0.544447107731532, 0.382565448778586]])
+
+        np.testing.assert_almost_equal(result, expected)
+
+    def test_coord_transform_downlooking(self):
+
+        T = self.T.copy()
+        T[1, :] = -T[1, :]
+        T[2, :] = -T[2, :]
+
+        u, v, w = stglib.aqd.qaqc.coord_transform(self.vel1,
+                                                  self.vel2,
+                                                  self.vel3,
+                                                  self.h,
+                                                  self.p,
+                                                  self.r,
+                                                  T,
+                                                  'BEAM')
+
+        result = np.hstack((u, v, w))
+        expected = np.array([[-0.530273437500000, -0.205039062500000, -0.374726562500000],
+                             [-0.581528098781915, -0.135532612145337, -0.327271926208413],
+                             [0.457413978956800, -0.281857021448140, -0.418306112347528]])
+
+        np.testing.assert_almost_equal(result, expected)
+
+
 if __name__ == '__main__':
     unittest.main()
