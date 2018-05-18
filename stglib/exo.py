ac56aea6e0b25308b4af7200da9717b272d52639
Dan Nowacki <dnowacki@usgs.gov>
2018-05-18 16:19:02 -0400

    Functionalize trimming code

--- stglib/exo.py
+++ stglib/exo.py
@@ -358,89 +358,124 @@ def exo_qaqc(ds):
     """
 
     for var in ['C_51', 'SpC_48', 'S_41', 'Turb']:
-        if var + '_min_diff' in ds.attrs:
-            print('%s: Trimming using minimum diff of %f' %
-                  (var, ds.attrs[var + '_min_diff']))
-            ds[var][np.ediff1d(
-                ds[var], to_begin=0) < ds.attrs[var + '_min_diff']] = np.nan
+        ds = trim_min_diff(ds, var)
 
-            notetxt = ('Values filled where data decreases by more than %f '
-                       'units in a single time step. ' %
-                       ds.attrs[var + '_min_diff'])
+        ds = trim_max_diff(ds, var)
 
-            if 'note' in ds[var].attrs:
-                ds[var].attrs['note'] = notetxt + ds[var].attrs['note']
+        ds = trim_med_diff(ds, var)
+
+        ds = trim_med_diff_pct(ds, var)
+
+        ds = trim_bad_ens(ds, var)
+
+    return ds
+
+
+def trim_min_diff(ds, var):
+    if var + '_min_diff' in ds.attrs:
+        print('%s: Trimming using minimum diff of %f' %
+              (var, ds.attrs[var + '_min_diff']))
+        ds[var][np.ediff1d(
+            ds[var], to_begin=0) < ds.attrs[var + '_min_diff']] = np.nan
+
+        notetxt = ('Values filled where data decreases by more than %f '
+                   'units in a single time step. ' %
+                   ds.attrs[var + '_min_diff'])
+
+        ds = insert_note(ds, var, notetxt)
+
+    return ds
+
+
+def trim_max_diff(ds, var):
+    if var + '_max_diff' in ds.attrs:
+        print('%s: Trimming using maximum diff of %f' %
+              (var, ds.attrs[var + '_max_diff']))
+        ds[var][np.ediff1d(
+            ds[var], to_begin=0) > ds.attrs[var + '_max_diff']] = np.nan
+
+        notetxt = ('Values filled where data increases by more than %f '
+                   'units in a single time step. ' %
+                   ds.attrs[var + '_max_diff'])
+
+        ds = insert_note(ds, var, notetxt)
+
+    return ds
+
+
+def trim_med_diff(ds, var):
+    if var + '_med_diff' in ds.attrs:
+        if 'kernel_size' in ds.attrs:
+            kernel_size = ds.attrs['kernel_size']
+        else:
+            kernel_size = 5
+        print('%s: Trimming using %d-point median filter diff of %f' %
+              (var, kernel_size, ds.attrs[var + '_med_diff']))
+        filtered = scipy.signal.medfilt(ds[var], kernel_size=kernel_size)
+        bads = np.abs(ds[var] - filtered) > ds.attrs[var + '_med_diff']
+        ds[var][bads] = np.nan
+
+        notetxt = ('Values filled where difference between %d-point '
+                   'median filter and original values is greater than '
+                   '%f. ' %
+                   (kernel_size, ds.attrs[var + '_med_diff']))
+
+        ds = insert_note(ds, var, notetxt)
+
+    return ds
+
+
+def trim_med_diff_pct(ds, var):
+    if var + '_med_diff_pct' in ds.attrs:
+        if 'kernel_size' in ds.attrs:
+            kernel_size = ds.attrs['kernel_size']
+        else:
+            kernel_size = 5
+        print('%s: Trimming using %d-point median filter diff of %f pct' %
+              (var, kernel_size, ds.attrs[var + '_med_diff_pct']))
+        filtered = scipy.signal.medfilt(ds[var], kernel_size=kernel_size)
+        bads = (100 * np.abs(ds[var] - filtered)/ds[var] >
+                ds.attrs[var + '_med_diff_pct'])
+        ds[var][bads] = np.nan
+
+        notetxt = ('Values filled where percent difference between '
+                   '%d-point median filter and original values is greater '
+                   'than %f. ' %
+                   (kernel_size, ds.attrs[var + '_med_diff_pct']))
+
+        ds = insert_note(ds, var, notetxt)
+
+    return ds
+
+
+def trim_bad_ens(ds, var):
+    if var + '_bad_ens' in ds.attrs:
+        inc = np.arange(0, len(ds.attrs[var + '_bad_ens']), 2)
+        for n in inc:
+            print('%s: Trimming using bad_ens %s' %
+                  (var, str(ds.attrs[var + '_bad_ens'][n:n+2])))
+            if isinstance(ds.attrs[var + '_bad_ens'][n], str):
+                bads = ((ds['time'] >= np.datetime64(ds.attrs[var + '_bad_ens'][n])) &
+                        (ds['time'] <= np.datetime64(ds.attrs[var + '_bad_ens'][n+1])))
+                ds[var] = ds[var].where(~bads)
             else:
-                ds[var].attrs.update({'note': notetxt})
-
-        if var + '_max_diff' in ds.attrs:
-            print('%s: Trimming using maximum diff of %f' %
-                  (var, ds.attrs[var + '_max_diff']))
-            ds[var][np.ediff1d(
-                ds[var], to_begin=0) > ds.attrs[var + '_max_diff']] = np.nan
-
-            notetxt = ('Values filled where data increases by more than %f '
-                       'units in a single time step. ' %
-                       ds.attrs[var + '_max_diff'])
-
-            if 'note' in ds[var].attrs:
-                ds[var].attrs['note'] = notetxt + ds[var].attrs['note']
-            else:
-                ds[var].attrs.update({'note': notetxt})
-
-        if var + '_med_diff' in ds.attrs:
-            if 'kernel_size' in ds.attrs:
-                kernel_size = ds.attrs['kernel_size']
-            else:
-                kernel_size = 5
-            print('%s: Trimming using %d-point median filter diff of %f' %
-                  (var, kernel_size, ds.attrs[var + '_med_diff']))
-            filtered = scipy.signal.medfilt(ds[var], kernel_size=kernel_size)
-            bads = np.abs(ds[var] - filtered) > ds.attrs[var + '_med_diff']
-            ds[var][bads] = np.nan
-
-            notetxt = ('Values filled where difference between %d-point '
-                       'median filter and original values is greater than '
-                       '%f. ' %
-                       (kernel_size, ds.attrs[var + '_med_diff']))
-
-            if 'note' in ds[var].attrs:
-                ds[var].attrs['note'] = notetxt + ds[var].attrs['note']
-            else:
-                ds[var].attrs.update({'note': notetxt})
-
-        if var + '_med_diff_pct' in ds.attrs:
-            if 'kernel_size' in ds.attrs:
-                kernel_size = ds.attrs['kernel_size']
-            else:
-                kernel_size = 5
-            print('%s: Trimming using %d-point median filter diff of %f pct' %
-                  (var, kernel_size, ds.attrs[var + '_med_diff_pct']))
-            filtered = scipy.signal.medfilt(ds[var], kernel_size=kernel_size)
-            bads = (100 * np.abs(ds[var] - filtered)/ds[var] >
-                    ds.attrs[var + '_med_diff_pct'])
-            ds[var][bads] = np.nan
-
-            notetxt = ('Values filled where percent difference between '
-                       '%d-point median filter and original values is greater '
-                       'than %f. ' %
-                       (kernel_size, ds.attrs[var + '_med_diff_pct']))
-
-            if 'note' in ds[var].attrs:
-                ds[var].attrs['note'] = notetxt + ds[var].attrs['note']
-            else:
-                ds[var].attrs.update({'note': notetxt})
-
-        if var + '_bad_ens' in ds.attrs:
-            inc = np.arange(0, len(ds.attrs[var + '_bad_ens']), 2)
-            for n in inc:
-                print('%s: Trimming using bad_ens %d:%d' %
-                      (var,
-                       ds.attrs[var + '_bad_ens'][n],
-                       ds.attrs[var + '_bad_ens'][n+1]))
                 bads = np.full(ds[var].shape, False)
                 bads[np.arange(ds.attrs[var + '_bad_ens'][n],
                                ds.attrs[var + '_bad_ens'][n+1])] = True
                 ds[var][bads] = np.nan
+
+            notetxt = "Data clipped using bad_ens values of %s. " % (
+                str(ds.attrs[var + '_bad_ens']))
+
+            ds = insert_note(ds, var, notetxt)
+
+    return ds
+
+
+def insert_note(ds, var, notetxt):
+    if 'note' in ds[var].attrs:
+        ds[var].attrs['note'] = notetxt + ds[var].attrs['note']
+    else:
+        ds[var].attrs.update({'note': notetxt})
 
     return ds
