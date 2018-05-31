94531661dfca397a97fc37d18377258ad2e00113
Dan Nowacki <dnowacki@usgs.gov>
2018-05-31 11:45:24 -0400

    Don't add diwasp history when we don't use diwasp

--- stglib/rsk/nc2waves.py
+++ stglib/rsk/nc2waves.py
@@ -39,8 +39,6 @@ def nc_to_waves(nc_filename):
     # assign min/max (need to do this after trimming):
     ds = utils.add_min_max(ds)
 
-    ds = utils.ds_add_diwasp_history(ds)
-
     nc_filename = ds.attrs['filename'] + 's-a.nc'
 
     ds = utils.rename_time(ds)
