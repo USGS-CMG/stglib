7faaa6b4a2c667c284d3226fc215a683679244f0
Dan Nowacki <dnowacki@usgs.gov>
2018-10-02 12:19:54 -0700

    Read argonaut velocites

--- stglib/argonaut.py
+++ stglib/argonaut.py
@@ -29,6 +29,26 @@ def read_dat_raw(filnam):
 def read_dat(filnam):
     return read_dat_raw(filnam)
 
+def read_vel(filnam):
+    df = pd.read_csv(filnam,
+                     delim_whitespace=True,
+                     header=[0,1],
+                     parse_dates=[[1,2,3,4,5,6]],
+                     date_parser=qaqc.date_parser)
+    df.rename(columns='_'.join, inplace=True)
+    df.rename(columns={"(_'_Y_'_,_ _'_(_)_'_)___(_'_M_'_,_ _'_(_)_'_)___(_'_D_'_,_ _'_(_)_'_)___(_'_H_'_,_ _'_(_)_'_)___(_'_M_'_,_ _'_(_)_'_)_._1___(_'_S_'_,_ _'_(_)_'_)":
+            'time'},
+            index=str,
+            inplace=True)
+    # df.columns = df.columns.str.replace('(cm/s)', '')
+    # df.columns = df.columns.str.replace('(deg)', '')
+    # df.columns = df.columns.str.replace('()', '')
+    df.columns = df.columns.str.replace(r"\(.*\)","")
+    df.set_index('time',inplace=True)
+    ds = xr.Dataset(df)
+    
+    return df
+
 def read_ctl(filnam):
     with open( filnam) as f:
         row = ''
