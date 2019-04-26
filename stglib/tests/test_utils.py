04a6f413cc8872843c255f98ead4561ab7214a6a
Dan Nowacki <dnowacki@usgs.gov>
2019-04-26 13:30:54 -0700

    Testing for clip_ds

--- stglib/tests/test_utils.py
+++ stglib/tests/test_utils.py
@@ -32,3 +32,47 @@ class TestUtils(unittest.TestCase):
         expected = (-1, 0)
         result = stglib.aqd.qaqc.rotate(0, 1, 270)
         np.testing.assert_almost_equal(expected, result)
+
+
+class TestClip(unittest.TestCase):
+
+    def setUp(self):
+        self.ds = xr.Dataset()
+        self.ds['time'] = xr.DataArray(
+            pd.date_range('2000-01-01 00:00',
+                          '2000-01-30 00:00',
+                          freq='15min'), dims='time')
+        self.ds.attrs['Deployment_date'] = '2000-01-01 00:05'
+        self.ds.attrs['Recovery_date'] = '2000-01-29 23:00'
+
+    def test_clip(self):
+        expected = xr.Dataset()
+        expected['time'] = xr.DataArray(
+            pd.date_range('2000-01-01 00:15',
+                          '2000-01-29 23:00',
+                          freq='15min'), dims='time')
+        result = stglib.utils.clip_ds(self.ds)
+
+        np.testing.assert_array_equal(expected['time'], result['time'])
+
+    def test_clip_good_dates(self):
+        expected = xr.Dataset()
+        expected['time'] = xr.DataArray(
+            pd.date_range('2000-01-10 15:45',
+                          '2000-01-19 00:00',
+                          freq='15min'), dims='time')
+        self.ds.attrs['good_dates'] = ['2000-01-10 15:41', '2000-01-19 00:00']
+        result = stglib.utils.clip_ds(self.ds)
+
+        np.testing.assert_array_equal(expected['time'], result['time'])
+
+    def test_clip_good_ens(self):
+        expected = xr.Dataset()
+        expected['time'] = xr.DataArray(
+            pd.date_range('2000-01-01 00:15',
+                          '2000-01-01 00:30',
+                          freq='15min'), dims='time')
+        self.ds.attrs['good_ens'] = [1, 3]
+        result = stglib.utils.clip_ds(self.ds)
+
+        np.testing.assert_array_equal(expected['time'], result['time'])
