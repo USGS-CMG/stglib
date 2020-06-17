a1199e0c204930d38188dd00c0fb3ca856c31644
Dan Nowacki <dnowacki@usgs.gov>
2020-06-17 10:58:48 -0700

    Expose qaqc to stglib imports

--- stglib/__init__.py
+++ stglib/__init__.py
@@ -2,6 +2,7 @@ from . import aqd, argonaut, core, eco, exo, hobo, indexvel, iq, rsk, troll
 from ._version import get_versions
 from .core import cmd, utils, waves
 from .core.utils import read_globalatts
+from .aqd import qaqc
 
 __version__ = get_versions()["version"]
 del get_versions
