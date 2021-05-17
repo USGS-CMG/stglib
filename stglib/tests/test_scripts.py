e8c9f277fc6e22489326ad0b7502e7ff3f3aa1d0
Dan Nowacki <dnowacki@usgs.gov>
2021-05-17 14:31:13 -0700

    Fix script paths

--- stglib/tests/test_scripts.py
+++ stglib/tests/test_scripts.py
@@ -4,14 +4,16 @@ import pytest
 
 def exo_raw(glob_att, config_yaml):
     result = subprocess.run(
-        ["runexocsv2cdf.py", glob_att, config_yaml], capture_output=True, cwd="data",
+        ["runexocsv2cdf.py", glob_att, config_yaml],
+        capture_output=True,
+        cwd="stglib/tests/data",
     )
     assert "Finished writing data" in result.stdout.decode("utf8")
 
 
 def exo_nc(nc_file):
     result = subprocess.run(
-        ["runexocdf2nc.py", nc_file], capture_output=True, cwd="data",
+        ["runexocdf2nc.py", nc_file], capture_output=True, cwd="stglib/tests/data",
     )
     assert "Done writing netCDF file" in result.stdout.decode("utf8")
 
