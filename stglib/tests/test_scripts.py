62334e4be8f9642ee8e80a6fe4ea8da596edd162
Dan Nowacki <dnowacki@usgs.gov>
2021-06-25 14:02:01 -0700

    Add AQD tests

--- stglib/tests/test_scripts.py
+++ stglib/tests/test_scripts.py
@@ -20,8 +20,29 @@ def exo_nc(nc_file):
     assert "Done writing netCDF file" in result.stdout.decode("utf8")
 
 
+def aqd_raw(glob_att, config_yaml):
+    result = subprocess.run(
+        ["python", "../../../scripts/runaqdhdr2cdf.py", glob_att, config_yaml],
+        capture_output=True,
+        cwd="stglib/tests/data",
+    )
+    assert "Finished writing data" in result.stdout.decode("utf8")
+
+
+def aqd_nc(nc_file):
+    result = subprocess.run(
+        ["python", "../../../scripts/runaqdcdf2nc.py", nc_file],
+        capture_output=True,
+        cwd="stglib/tests/data",
+    )
+    assert "Done writing netCDF file" in result.stdout.decode("utf8")
+
 def test_exo():
     exo_raw("glob_attbel5C.txt", "config_bel5C.yaml")
     exo_nc("bel53Cexo-raw.cdf")
     exo_raw("glob_att1119a.txt", "1119Aexo_config.yaml")
     exo_nc("1119Aexo-raw.cdf")
+
+def test_aqd():
+    aqd_raw("glob_att1118a_b.txt", "aqd1118A_config.yaml")
+    aqd_nc("1118ABaqd-raw.cdf")
