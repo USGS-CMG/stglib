9d901c0f881a48d53ffe8c552f5d9ebfc7682679
De Meo <odemeo@usgs.gov>
2022-03-22 10:31:35 -0400

    Added wxt test files

--- stglib/tests/test_scripts.py
+++ stglib/tests/test_scripts.py
@@ -46,3 +46,23 @@ def test_exo():
 def test_aqd():
     aqd_raw("glob_att1118a_b.txt", "aqd1118A_config.yaml")
     aqd_nc("1118ABaqd-raw.cdf")
+    
+def wxt_raw(glob_att, config_yaml):
+    result = subprocess.run(
+        ["python", "../../../scripts/runwxtcsv2cdf.py", glob_att, config_yaml],
+        capture_output=True,
+        cwd="stglib/tests/data",
+    )
+    assert "Finished writing data" in result.stdout.decode("utf8")
+
+def wxt_nc(nc_file):
+    result = subprocess.run(
+        ["python", "../../../scripts/runwxtcdf2nc.py", nc_file],
+        capture_output=True,
+        cwd="stglib/tests/data",
+    )
+    assert "Done writing netCDF file" in result.stdout.decode("utf8")
+    
+def test_wxt():
+    wxt_raw("glob_att1149.txt", "wxt1149_config.yaml")
+    wxt_nc("1149wxt-raw.cdf")
