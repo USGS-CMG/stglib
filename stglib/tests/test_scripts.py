377276231497d9ed28b39e9d5af109e6097bad0d
De Meo <odemeo@usgs.gov>
2022-03-23 11:00:50 -0400

    Blacked code and added Git LFS

--- stglib/tests/test_scripts.py
+++ stglib/tests/test_scripts.py
@@ -37,16 +37,19 @@ def aqd_nc(nc_file):
     )
     assert "Done writing netCDF file" in result.stdout.decode("utf8")
 
+
 def test_exo():
     exo_raw("glob_attbel5C.txt", "config_bel5C.yaml")
     exo_nc("bel53Cexo-raw.cdf")
     exo_raw("glob_att1119a.txt", "1119Aexo_config.yaml")
     exo_nc("1119Aexo-raw.cdf")
 
+
 def test_aqd():
     aqd_raw("glob_att1118a_b.txt", "aqd1118A_config.yaml")
     aqd_nc("1118ABaqd-raw.cdf")
-    
+
+
 def wxt_raw(glob_att, config_yaml):
     result = subprocess.run(
         ["python", "../../../scripts/runwxtcsv2cdf.py", glob_att, config_yaml],
@@ -55,6 +58,7 @@ def wxt_raw(glob_att, config_yaml):
     )
     assert "Finished writing data" in result.stdout.decode("utf8")
 
+
 def wxt_nc(nc_file):
     result = subprocess.run(
         ["python", "../../../scripts/runwxtcdf2nc.py", nc_file],
@@ -62,7 +66,8 @@ def wxt_nc(nc_file):
         cwd="stglib/tests/data",
     )
     assert "Done writing netCDF file" in result.stdout.decode("utf8")
-    
+
+
 def test_wxt():
     wxt_raw("glob_att1149.txt", "wxt1149_config.yaml")
     wxt_nc("1149wxt-raw.cdf")
