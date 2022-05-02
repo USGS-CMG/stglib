3ff198e0dbf80b63af11b36870bae5b14dd7c5b9
Dan Nowacki <dnowacki@usgs.gov>
2022-05-02 10:08:24 -0700

    Update versioneer

--- stglib/__init__.py
+++ stglib/__init__.py
@@ -1,8 +1,8 @@
 from . import aqd, argonaut, core, eco, exo, hobo, indexvel, iq, rdi, rsk, troll
-from ._version import get_versions
 from .core import cmd, utils, waves
 from .core.utils import read_globalatts
 from .aqd import aqdutils
 
-__version__ = get_versions()["version"]
-del get_versions
+from . import _version
+
+__version__ = _version.get_versions()["version"]
