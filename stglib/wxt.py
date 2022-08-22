a2dd4b24174b2a6d779432f7146d839c92b7198c
ssuttles-usgs <ssuttles@usgs.gov>
2022-08-22 07:36:33 -0400

    fix dtype issue v2

--- stglib/wxt.py
+++ stglib/wxt.py
@@ -87,15 +87,23 @@ def cdf_to_nc(cdf_filename):
     # If sensor wasn't pointing to magnetic north, apply offset to direction
     if "dir_offset" in ds.attrs:
         if "WD_min" in ds:
-            ds["WD_min"].values = ds["WD_min"].values + ds.attrs["dir_offset"].astype(float)
+            ds["WD_min"].values = ds["WD_min"].values + ds.attrs["dir_offset"].astype(
+                float
+            )
         if "WD_410" in ds:
-            ds["WD_410"].values = ds["WD_410"].values + ds.attrs["dir_offset"].astype(float)
+            ds["WD_410"].values = ds["WD_410"].values + ds.attrs["dir_offset"].astype(
+                float
+            )
         if "WD_gust" in ds:
-            ds["WD_gust"].values = ds["WD_gust"].values + ds.attrs["dir_offset"].astype(float)
+            ds["WD_gust"].values = ds["WD_gust"].values + ds.attrs["dir_offset"].astype(
+                float
+            )
 
     # Convert direction from magnetic to true with magenetic declination
     if "WD_min" in ds:
-        ds["WD_min"].values = ds["WD_min"].values + ds.attrs["magnetic_variation"].astype(float)
+        ds["WD_min"].values = ds["WD_min"].values + ds.attrs[
+            "magnetic_variation"
+        ].astype(float)
         ds["WD_min"].values = ds["WD_min"].values.round(0)
         ds["WD_min"].values[ds["WD_min"].values < 0.0] = (
             ds["WD_min"].values[ds["WD_min"].values < 0.0] + 360.0
@@ -106,7 +114,9 @@ def cdf_to_nc(cdf_filename):
         ds["WD_min"].values[ds["WD_min"].values == 0.0] = 0.0  # convert any -0. to 0.
 
     if "WD_410" in ds:
-        ds["WD_410"].values = ds["WD_410"].values + ds.attrs["magnetic_variation"].astype(float)
+        ds["WD_410"].values = ds["WD_410"].values + ds.attrs[
+            "magnetic_variation"
+        ].astype(float)
         ds["WD_410"].values = ds["WD_410"].values.round(0)
         ds["WD_410"].values[ds["WD_410"].values < 0.0] = (
             ds["WD_410"].values[ds["WD_410"].values < 0.0] + 360.0
@@ -117,7 +127,9 @@ def cdf_to_nc(cdf_filename):
         ds["WD_410"].values[ds["WD_410"].values == 0.0] = 0.0  # convert any -0. to 0.
 
     if "WD_gust" in ds:
-        ds["WD_gust"].values = ds["WD_gust"].values + ds.attrs["magnetic_variation"].astype(float)
+        ds["WD_gust"].values = ds["WD_gust"].values + ds.attrs[
+            "magnetic_variation"
+        ].astype(float)
         ds["WD_gust"].values = ds["WD_gust"].values.round(0)
         ds["WD_gust"].values[ds["WD_gust"].values < 0.0] = (
             ds["WD_gust"].values[ds["WD_gust"].values < 0.0] + 360.0
