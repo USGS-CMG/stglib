bb22ae2203406d1a2a742a18e9f47f2c6aa49adc
Dan Nowacki <dnowacki@usgs.gov>
2022-09-30 11:58:38 -0700

    Refactor waves CDF to NC

--- stglib/aqd/wvscdf2nc.py
+++ stglib/aqd/wvscdf2nc.py
@@ -1,6 +1,8 @@
 from ..core import utils
 from . import aqdutils
+from . import cdf2nc
 import datetime
+import xarray as xr
 
 
 def cdf_to_nc(cdf_filename, atmpres=False, writefile=True):
@@ -20,6 +22,11 @@ def cdf_to_nc(cdf_filename, atmpres=False, writefile=True):
 
     # Transform coordinates from ENU to BEAM if necessary
     if "wave_coord_output" in ds.attrs:
+        if ds.attrs["wave_coord_output"] != "ENU":
+            raise NotImplementedError(
+                "Only wave_coord_output to ENU is supported at this time"
+            )
+
         histtext = "{}: Converting from {} to {} at user request.\n".format(
             datetime.datetime.now(datetime.timezone.utc).isoformat(),
             ds.attrs["AQDCoordinateSystem"],
@@ -39,37 +46,43 @@ def cdf_to_nc(cdf_filename, atmpres=False, writefile=True):
             ds.attrs["AQDCoordinateSystem"],
             out=ds.attrs["wave_coord_output"],
         )
-        ds["VEL1"].values = u
-        ds["VEL2"].values = v
-        ds["VEL3"].values = w
+        ds["U"] = xr.DataArray(u, dims=("time", "sample"))
+        ds["V"] = xr.DataArray(v, dims=("time", "sample"))
+        ds["W"] = xr.DataArray(w, dims=("time", "sample"))
+
+        ds = ds.drop(["VEL1", "VEL2", "VEL3"])
 
     # Make bin_depth variable
     ds = aqdutils.make_bin_depth(ds, waves=True)
 
     # Swap dimensions from bindist to depth
-    ds = aqdutils.swap_bindist_to_depth(ds)
+    # ds = aqdutils.swap_bindist_to_depth(ds)
+    ds = cdf2nc.ds_swap_dims(ds)
     # Rename DataArrays within Dataset for EPIC compliance
     # and append depth coord to velocities and amplitudes
     ds = aqdutils.ds_rename(ds, waves=True)
-
     # add EPIC and CMG attributes, set _FillValue
     ds = aqdutils.ds_add_attrs(ds, waves=True)
+
+    # Add minimum and maximum attributes
+    ds = utils.add_min_max(ds)
 
     # Add DELTA_T for EPIC compliance
     ds = aqdutils.add_delta_t(ds, waves=True)
 
-    # Add minimum and maximum attributes
-    ds = utils.add_min_max(ds)
+    # Add start_time and stop_time attrs
+    ds = utils.add_start_stop_time(ds)
+
+    # Add history showing file used
+    ds = utils.add_history(ds)
+
+    ds = utils.add_standard_names(ds)
 
     # Cast vars as float32
     for var in ds.variables:
         if (var not in ds.coords) and ("time" not in var):
             # cast as float32
             ds = utils.set_var_dtype(ds, var)
-
-    # Need to add lat lon to certain variables
-    # for var in ["Hdg_1215", "Ptch_1216", "Roll_1217"]:
-    #     ds = utils.add_lat_lon(ds, var)
 
     # Ensure no _FillValue is assigned to coordinates
     ds = utils.ds_coord_no_fillvalue(ds)
