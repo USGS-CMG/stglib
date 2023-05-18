5806afcbec7a612e834e85d49fa3b15bd291a1f3
Zafer <zdefne-usgs@users.noreply.github.com>
2023-05-18 12:26:31 -0400

    Update pre_uninstall.py

--- stglib/pre_uninstall.py
+++ stglib/pre_uninstall.py
@@ -1,8 +1,9 @@
 import os
+from pathlib import Path
 
 def remove_from_path():
     package_dir = os.path.dirname(os.path.abspath(__file__))
-    path_to_remove = os.path.join(package_dir, '..', '..', '..', scripts')
+    path_to_remove = str(Path(package_dir).parents[3].joinpath('scripts'))
 
     # Remove the package directory from the PATH environment variable
     paths = os.environ['PATH'].split(':')
