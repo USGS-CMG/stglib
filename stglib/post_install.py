a3e60783df8e0584f6c9ec8a2c853666b3095a43
Zafer <zdefne-usgs@users.noreply.github.com>
2023-05-18 12:25:51 -0400

    Update post_install.py

--- stglib/post_install.py
+++ stglib/post_install.py
@@ -1,8 +1,9 @@
 import os
+from pathlib import Path
 
 def update_path():
     package_dir = os.path.dirname(os.path.abspath(__file__))
-    new_path = os.path.join(package_dir, '..', '..', '..', 'scripts')
+    new_path = str(Path(package_dir).parents[3].joinpath('scripts'))
 
     # Modify the PATH environment variable
     os.environ['PATH'] = f"{new_path}:{os.environ['PATH']}"
