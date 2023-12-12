618217e04a3b6024c42d6ff96c67f426749612f5
Dan Nowacki <dnowacki@usgs.gov>
2023-12-12 10:36:25 -0800

    Simplify command-line argument parsing (#166)

--- stglib/core/cmd.py
+++ stglib/core/cmd.py
@@ -3,21 +3,32 @@ import argparse
 
 def yamlarg(parser):
     parser.add_argument(
-        "config", help=("path to instrument configuration file" " (YAML formatted)")
+        "config", help="path to instrument configuration file (YAML formatted)"
     )
 
 
 def gattsarg(parser):
     parser.add_argument(
-        "gatts", help=("path to global attributes file (gatts " "formatted)")
+        "gatts", help="path to global attributes file (gatts formatted)"
     )
+
+
+def cdfarg(parser):
+    parser.add_argument("cdfname", help="raw .cdf filename")
+
+
+def atmarg(parser):
+    parser.add_argument(
+        "--atmpres", help="path to cdf file containing atmopsheric pressure data"
+    )
+
+
+def ncarg(parser):
+    parser.add_argument("ncname", help="processed .nc filename")
 
 
 def aqdhdr2cdf_parser():
-    description = (
-        "Convert Aquadopp text files to raw .cdf format. Run this "
-        "script from the directory containing Aquadopp files"
-    )
+    description = "Convert Aquadopp text files to raw .cdf format. Run this script from the directory containing Aquadopp files."
     parser = argparse.ArgumentParser(description=description)
     gattsarg(parser)
     yamlarg(parser)
@@ -26,25 +37,16 @@ def aqdhdr2cdf_parser():
 
 
 def aqdcdf2nc_parser():
-    description = (
-        "Convert raw Aquadopp .cdf format to processed .nc files, "
-        "optionally compensating for atmospheric pressure"
-    )
+    description = "Convert raw Aquadopp .cdf format to processed .nc files, optionally compensating for atmospheric pressure"
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("cdfname", help="raw .CDF filename")
-    parser.add_argument(
-        "--atmpres", help=("path to cdf file containing atmopsheric pressure data")
-    )
+    cdfarg(parser)
+    atmarg(parser)
 
     return parser
 
 
 def wvswad2cdf_parser():
-    description = (
-        "Convert Aquadopp .wad wave files to raw .cdf format. "
-        "Run this script from the directory containing Aquadopp "
-        "files"
-    )
+    description = "Convert Aquadopp .wad wave files to raw .cdf format. Run this script from the directory containing Aquadopp files."
     parser = argparse.ArgumentParser(description=description)
     gattsarg(parser)
     yamlarg(parser)
@@ -55,10 +57,8 @@ def wvswad2cdf_parser():
 def wvscdf2nc_parser():
     description = "Convert raw Aquadopp .cdf wave files to processed .nc files"
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("cdfname", help="raw .CDF filename")
-    parser.add_argument(
-        "--atmpres", help=("path to cdf file containing atmopsheric pressure data")
-    )
+    cdfarg(parser)
+    atmarg(parser)
 
     return parser
 
@@ -66,7 +66,7 @@ def wvscdf2nc_parser():
 def wvsnc2diwasp_parser():
     description = "Convert processed Aquadopp waves .nc files using DIWASP"
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("ncname", help="processed .nc filename")
+    ncarg(parser)
 
     return parser
 
@@ -74,17 +74,13 @@ def wvsnc2diwasp_parser():
 def wvsnc2waves_parser():
     description = "Generate waves statistics file"
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("ncname", help="processed .nc filename")
+    ncarg(parser)
 
     return parser
 
 
 def rskcsv2cdf_parser():
-    description = (
-        "Convert exported RBR solo csv files to raw .cdf format. "
-        "Run this script from the directory "
-        "containing the files"
-    )
+    description = "Convert exported RBR csv files to raw .cdf format. Run this script from the directory containing the files."
     parser = argparse.ArgumentParser(description=description)
     gattsarg(parser)
     yamlarg(parser)
@@ -93,11 +89,7 @@ def rskcsv2cdf_parser():
 
 
 def rskrsk2cdf_parser():
-    description = (
-        "Convert raw RBR d|wave files (.rsk) to raw .cdf format. "
-        "Run this script from the directory "
-        "containing d|wave files"
-    )
+    description = "Convert raw RBR files (.rsk) to raw .cdf format. Run this script from the directory containing the files."
     parser = argparse.ArgumentParser(description=description)
     gattsarg(parser)
     yamlarg(parser)
@@ -106,15 +98,10 @@ def rskrsk2cdf_parser():
 
 
 def rskcdf2nc_parser():
-    description = (
-        "Convert raw RBR d|wave .cdf format to processed .nc files,"
-        " optionally compensating for atmospheric pressure"
-    )
+    description = "Convert raw RBR d|wave .cdf format to processed .nc files, optionally compensating for atmospheric pressure"
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("cdfname", help="raw .CDF filename")
-    parser.add_argument(
-        "--atmpres", help=("path to cdf file containing atmopsheric pressure data")
-    )
+    cdfarg(parser)
+    atmarg(parser)
 
     return parser
 
@@ -122,7 +109,7 @@ def rskcdf2nc_parser():
 def rsknc2diwasp_parser():
     description = "Convert processed .nc files using DIWASP"
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("ncname", help="processed .nc filename")
+    ncarg(parser)
 
     return parser
 
@@ -130,16 +117,13 @@ def rsknc2diwasp_parser():
 def rsknc2waves_parser():
     description = "Generate waves statistics file"
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("ncname", help="processed .nc filename")
+    ncarg(parser)
 
     return parser
 
 
 def hwlbcsv2cdf_parser():
-    description = (
-        "Convert HOBO pressure sensor .csv file to raw .cdf format."
-        " Run this script from the directory containing IQ file"
-    )
+    description = "Convert HOBO pressure sensor .csv file to raw .cdf format. Run this script from the directory containing HOBO file."
     parser = argparse.ArgumentParser(description=description)
     gattsarg(parser)
     yamlarg(parser)
@@ -150,16 +134,13 @@ def hwlbcsv2cdf_parser():
 def hwlbcdf2nc_parser():
     description = "Convert raw HOBO .cdf format to processed .nc files"
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("cdfname", help="raw .CDF filename")
+    cdfarg(parser)
 
     return parser
 
 
 def iqmat2cdf_parser():
-    description = (
-        "Convert SonTek IQ .mat file to raw .cdf format. Run this "
-        "script from the directory containing IQ file"
-    )
+    description = "Convert SonTek IQ .mat file to raw .cdf format. Run this script from the directory containing IQ file."
     parser = argparse.ArgumentParser(description=description)
     gattsarg(parser)
     yamlarg(parser)
@@ -170,16 +151,13 @@ def iqmat2cdf_parser():
 def iqcdf2nc_parser():
     description = "Convert raw SonTek IQ .cdf format to processed .nc files"
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("cdfname", help="raw .CDF filename")
+    cdfarg(parser)
 
     return parser
 
 
 def exocsv2cdf_parser():
-    description = (
-        "Convert EXO .csv file to raw .cdf format. Run this script "
-        "from the directory containing EXO file"
-    )
+    description = "Convert EXO .csv file to raw .cdf format. Run this script from the directory containing EXO file."
     parser = argparse.ArgumentParser(description=description)
     gattsarg(parser)
     yamlarg(parser)
@@ -190,19 +168,14 @@ def exocsv2cdf_parser():
 def exocdf2nc_parser():
     description = "Convert raw EXO .cdf format to processed .nc files"
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("cdfname", help="raw .CDF filename")
-    parser.add_argument(
-        "--atmpres", help=("path to cdf file containing atmopsheric pressure data")
-    )
+    cdfarg(parser)
+    atmarg(parser)
 
     return parser
 
 
 def lisstcsv2cdf_parser():
-    description = (
-        "Convert LISST .csv file to raw .cdf format. Run this script "
-        "from the directory containing LIST file"
-    )
+    description = "Convert LISST .csv file to raw .cdf format. Run this script from the directory containing LISST file."
     parser = argparse.ArgumentParser(description=description)
     gattsarg(parser)
     yamlarg(parser)
@@ -213,19 +186,14 @@ def lisstcsv2cdf_parser():
 def lisstcdf2nc_parser():
     description = "Convert raw LISST .cdf format to processed .nc files"
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("cdfname", help="raw .CDF filename")
-    parser.add_argument(
-        "--atmpres", help=("path to cdf file containing " "atmopsheric pressure data")
-    )
+    cdfarg(parser)
+    atmarg(parser)
 
     return parser
 
 
 def trollcsv2cdf_parser():
-    description = (
-        "Convert Aqua TROLL .csv file to raw .cdf format. Run this script "
-        "from the directory containing .csv file"
-    )
+    description = "Convert Aqua TROLL .csv file to raw .cdf format. Run this script from the directory containing .csv file."
     parser = argparse.ArgumentParser(description=description)
     gattsarg(parser)
     yamlarg(parser)
@@ -236,16 +204,13 @@ def trollcsv2cdf_parser():
 def trollcdf2nc_parser():
     description = "Convert raw Aqua TROLL .cdf format to processed .nc files"
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("cdfname", help="raw .CDF filename")
+    cdfarg(parser)
 
     return parser
 
 
 def ecolog2cdf_parser():
-    description = (
-        "Convert WET Labs ECO file to raw .cdf format. Run this "
-        "script from the directory containing ECO file"
-    )
+    description = "Convert WET Labs ECO file to raw .cdf format. Run this script from the directory containing ECO file."
     parser = argparse.ArgumentParser(description=description)
     gattsarg(parser)
     yamlarg(parser)
@@ -256,13 +221,13 @@ def ecolog2cdf_parser():
 def ecocdf2nc_parser():
     description = "Convert raw ECO .cdf format to processed .nc files"
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("cdfname", help="raw .CDF filename")
+    cdfarg(parser)
 
     return parser
 
 
 def rdiraw2cdf_parser():
-    description = "Convert RDI raw binary files to raw .cdf format. Run this script from the directory containing RDI files"
+    description = "Convert RDI raw binary files to raw .cdf format. Run this script from the directory containing RDI files."
     parser = argparse.ArgumentParser(description=description)
     gattsarg(parser)
     yamlarg(parser)
@@ -273,19 +238,14 @@ def rdiraw2cdf_parser():
 def rdicdf2nc_parser():
     description = "Convert raw RDI .cdf format to processed .nc files, optionally compensating for atmospheric pressure"
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("cdfname", help="raw .CDF filename")
-    parser.add_argument(
-        "--atmpres", help=("path to cdf file containing atmopsheric pressure data")
-    )
+    cdfarg(parser)
+    atmarg(parser)
 
     return parser
 
 
 def aqdturnaround_parser():
-    description = (
-        "Create Aquadopp turnaround plots. Run this script from "
-        "the directory containing Aquadopp files"
-    )
+    description = "Create Aquadopp turnaround plots. Run this script from the directory containing Aquadopp files."
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument("basefile", help="basename of AQD file (without extension)")
     parser.add_argument(
@@ -298,10 +258,7 @@ def aqdturnaround_parser():
 
 
 def exoturnaround_parser():
-    description = (
-        "Create YSI EXO turnaround plots. Run this script from "
-        "the directory containing EXO files"
-    )
+    description = "Create YSI EXO turnaround plots. Run this script from the directory containing EXO files."
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument(
         "basefile", help="basename of EXO .csv file (without extension)"
@@ -317,10 +274,7 @@ def exoturnaround_parser():
 
 
 def wxtcsv2cdf_parser():
-    description = (
-        "Convert Vaisala WXT met .csv file to raw .cdf format. Run this script "
-        "from the directory containing Vaisala .csv file"
-    )
+    description = "Convert Vaisala WXT met .csv file to raw .cdf format. Run this script from the directory containing Vaisala .csv file."
     parser = argparse.ArgumentParser(description=description)
     gattsarg(parser)
     yamlarg(parser)
@@ -331,16 +285,13 @@ def wxtcsv2cdf_parser():
 def wxtcdf2nc_parser():
     description = "Convert raw Vaisala WXT .cdf format to processed .nc files"
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("cdfname", help="raw .CDF filename")
+    cdfarg(parser)
 
     return parser
 
 
 def eofelog2cdf_parser():
-    description = (
-        "Convert EofE echologger .log file to raw .cdf format. Run this script "
-        "from the directory containing ea400 echologger .log file"
-    )
+    description = "Convert EofE echologger .log file to raw .cdf format. Run this script from the directory containing ea400 echologger .log file."
     parser = argparse.ArgumentParser(description=description)
     gattsarg(parser)
     yamlarg(parser)
@@ -351,13 +302,13 @@ def eofelog2cdf_parser():
 def eofecdf2nc_parser():
     description = "Convert raw echologger .cdf format to processed .nc files"
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("cdfname", help="raw .CDF filename")
+    cdfarg(parser)
 
     return parser
 
 
 def vechdr2cdf_parser():
-    description = "Convert Vector text files to raw .cdf format. Run this script from the directory containing Vector files"
+    description = "Convert Vector text files to raw .cdf format. Run this script from the directory containing Vector files."
     parser = argparse.ArgumentParser(description=description)
     gattsarg(parser)
     yamlarg(parser)
@@ -368,16 +319,14 @@ def vechdr2cdf_parser():
 def veccdf2nc_parser():
     description = "Convert raw Vector .cdf format to processed .nc files, optionally compensating for atmospheric pressure"
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("cdfname", help="raw .CDF filename")
-    parser.add_argument(
-        "--atmpres", help=("path to cdf file containing atmopsheric pressure data")
-    )
+    cdfarg(parser)
+    atmarg(parser)
 
     return parser
 
 
 def sigmat2cdf_parser():
-    description = "Convert Signature files exported in Matlab format to raw .cdf format. Run this script from the directory containing Signature files"
+    description = "Convert Signature files exported in Matlab format to raw .cdf format. Run this script from the directory containing Signature files."
     parser = argparse.ArgumentParser(description=description)
     gattsarg(parser)
     yamlarg(parser)
@@ -398,9 +347,7 @@ def sigcdf2nc_parser():
     description = "Convert raw Signature .cdf format to processed .nc files, optionally compensating for atmospheric pressure"
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument("cdfname", help="raw .CDF filename(s)", nargs="+")
-    parser.add_argument(
-        "--atmpres", help=("path to cdf file containing atmopsheric pressure data")
-    )
+    atmarg(parser)
 
     return parser
 
@@ -409,8 +356,6 @@ def sigdlfncdf2nc_parser():
     description = "Convert raw Signature .cdf format to processed .nc files, optionally compensating for atmospheric pressure"
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument("cdfname", help="raw .CDF filename(s)", nargs="+")
-    parser.add_argument(
-        "--atmpres", help=("path to cdf file containing atmopsheric pressure data")
-    )
+    atmarg(parser)
 
     return parser
