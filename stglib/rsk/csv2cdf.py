2faa5c1f6a54037aa283d922995ab4397eec3b58
Dan Nowacki <dnowacki@usgs.gov>
2023-12-15 16:17:52 -0800

    Improve lat/lon handling

--- stglib/rsk/csv2cdf.py
+++ stglib/rsk/csv2cdf.py
@@ -142,10 +142,14 @@ def csv_to_cdf(metadata):
             if len(ds.attrs["latitude"]) == len(rowsize) and len(
                 ds.attrs["longitude"]
             ) == len(rowsize):
-                ds["latitude"] = xr.DataArray(ds.attrs["latitude"], dims="profile")
-                ds["longitude"] = xr.DataArray(ds.attrs["longitude"], dims="profile")
-                ds.attrs["latitude"].pop()
-                ds.attrs["longitude"].pop()
+                ds["latitude"] = xr.DataArray(
+                    np.array(ds.attrs["latitude"]).astype(float), dims="profile"
+                )
+                ds["longitude"] = xr.DataArray(
+                    np.array(ds.attrs["longitude"]).astype(float), dims="profile"
+                )
+                ds.attrs.pop("latitude")
+                ds.attrs.pop("longitude")
             else:
                 raise ValueError(
                     f"size of latitude ({len(ds.attrs['latitude'])}) and longitude ({len(ds.attrs['longitude'])}) does not match number of profiles ({len(rowsize)})"
