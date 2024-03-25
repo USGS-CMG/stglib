93747043c9377a6533e1a2464d54621035b9cca9
Dan Nowacki <dnowacki@usgs.gov>
2024-03-25 13:25:17 -0700

    Functionize

--- stglib/aqd/wvsnc2waves.py
+++ stglib/aqd/wvsnc2waves.py
@@ -24,151 +24,7 @@ def nc_to_waves(nc_filename):
             dopuv = True
 
     if dopuv:
-        print("Computing PUV")
-        desc = {
-            "Hrmsp": "Hrms (=Hmo) from pressure",
-            "Hrmsu": "Hrms from u,v",
-            "ubr": "Representative orbital velocity amplitude in freq. band ( first_frequency_cutoff <= f <= last_frequency_cutoff ) (m/s)",
-            "omegar": "Representative orbital velocity (radian frequency)",
-            "Tr": "Representative orbital velocity period (s)",
-            "Tpp": "Peak period from pressure (s)",
-            "Tpu": "Peak period from velocity (s)",
-            "phir": "Representative orbital velocity direction (angles from x-axis, positive ccw)",
-            "azr": "Representative orb. velocity direction (deg; geographic azimuth; ambiguous =/- 180 degrees)",
-            "ublo": "ubr in freq. band (f <= first_frequency_cutoff) (m/s)",
-            "ubhi": "ubr in freq. band (f >= last_frequency_cutoff) (m/s)",
-            "ubig": "ubr in infra-gravity freq. band (first_frequency_cutoff f <= 1/20) (m/s)",
-            "Hrmsp_tail": "Hrms (=Hmo) from pressure with tail applied",
-            "Hrmsu_tail": "Hrms from u,v with tail applied",
-            "phir_tail": "Representative orbital velocity direction (angles from x-axis, positive ccw) with tail applied",
-            "azr_tail": "Representative orb. velocity direction (deg; geographic azimuth; ambiguous =/- 180 degrees) with tail applied",
-        }
-        standard_name = {
-            # "Hrmsp": "sea_surface_wave_significant_height",
-            # "Hrmsu": "sea_surface_wave_significant_height",
-            # "ubr": "Representative orbital velocity amplitude in freq. band ( first_frequency_cutoff <= f <= last_frequency_cutoff ) (m/s)",
-            # "omegar": "Representative orbital velocity (radian frequency)",
-            # "Tr": "Representative orbital velocity period (s)",
-            "Tpp": "sea_surface_wave_period_at_variance_spectral_density_maximum",
-            "Tpu": "sea_surface_wave_period_at_variance_spectral_density_maximum",
-            "phir": "sea_surface_wave_from_direction",
-            "azr": "sea_surface_wave_from_direction",
-            # "ublo": "ubr in freq. band (f <= first_frequency_cutoff) (m/s)",
-            # "ubhi": "ubr in freq. band (f >= last_frequency_cutoff) (m/s)",
-            # "ubig": "ubr in infra-gravity freq. band (first_frequency_cutoff f <= 1/20) (m/s)",
-            # "Hrmsp_tail": "sea_surface_wave_significant_height",
-            # "Hrmsu_tail": "sea_surface_wave_significant_height",
-            "phir_tail": "sea_surface_wave_from_direction",
-            "azr_tail": "sea_surface_wave_from_direction",
-        }
-        if ds.attrs["orientation"] != "UP":
-            raise NotImplementedError(
-                "PUV currently only works on UP oriented instruments"
-            )
-        if "u_1205" not in ds:  # in case we are already in ENU coordinates
-            # only works on UP because we assume T = T_orig
-            # need to pass .values because passing the DataArray is MUCH slower
-            u, v, w = aqdutils.coord_transform(
-                ds["vel1_1277"].squeeze().values,
-                ds["vel2_1278"].squeeze().values,
-                ds["vel3_1279"].squeeze().values,
-                ds["Hdg_1215"].squeeze().values,
-                ds["Ptch_1216"].squeeze().values,
-                ds["Roll_1217"].squeeze().values,
-                ds["TransMatrix"].squeeze().values,
-                ds["TransMatrix"].squeeze().values,
-                ds.attrs["AQDCoordinateSystem"],
-            )
-            ds["u_1205"] = xr.DataArray(u, dims=("time", "sample"))
-            ds["v_1206"] = xr.DataArray(v, dims=("time", "sample"))
-            ds["w_1204"] = xr.DataArray(w, dims=("time", "sample"))
-
-        N, M = np.shape(ds["u_1205"].squeeze())
-        puvs = {
-            k: np.full_like(ds["time"].values, np.nan, dtype=float)
-            for k in desc
-            # "Hrmsp": np.full_like(ds["time"].values, np.nan, dtype=float),
-            # "Hrmsu": np.full_like(ds["time"].values, np.nan, dtype=float),
-            # "ubr": np.full_like(ds["time"].values, np.nan, dtype=float),
-            # "omegar": np.full_like(ds["time"].values, np.nan, dtype=float),
-            # "Tr": np.full_like(ds["time"].values, np.nan, dtype=float),
-            # "Tpp": np.full_like(ds["time"].values, np.nan, dtype=float),
-            # "Tpu": np.full_like(ds["time"].values, np.nan, dtype=float),
-            # "phir": np.full_like(ds["time"].values, np.nan, dtype=float),
-            # "azr": np.full_like(ds["time"].values, np.nan, dtype=float),
-            # "ublo": np.full_like(ds["time"].values, np.nan, dtype=float),
-            # "ubhi": np.full_like(ds["time"].values, np.nan, dtype=float),
-            # "ubig": np.full_like(ds["time"].values, np.nan, dtype=float),
-            # "Hrmsp_tail": np.full_like(ds["time"].values, np.nan, dtype=float),
-            # "Hrmsu_tail": np.full_like(ds["time"].values, np.nan, dtype=float),
-        }
-
-        print("Running puv_quick")
-        for n in tqdm(range(N)):
-            try:
-                puv = waves.puv_quick(
-                    ds["P_1ac"][n, :].values,
-                    ds["u_1205"][n, :],
-                    ds["v_1206"][n, :],
-                    ds["P_1ac"][n, :].mean().values
-                    + ds.attrs["initial_instrument_height"],
-                    ds.attrs["initial_instrument_height"],
-                    ds.attrs["initial_instrument_height"]
-                    + ds.attrs["center_first_bin"],
-                    1 / ds.attrs["sample_interval"],
-                    first_frequency_cutoff=1 / 10,
-                    last_frequency_cutoff=1 / 2.5,
-                )
-                for k in puvs:
-                    puvs[k][n] = puv[k]
-
-            except (
-                AttributeError
-            ):  # puv_quick will fail on some values and return AttributeError: 'float' object has no attribute 'astype'
-                continue
-
-        for k in puvs:
-            ds["puv_" + k] = xr.DataArray(puvs[k], dims="time")
-            if k in desc:
-                ds["puv_" + k].attrs["description"] = desc[k]
-            if k in standard_name:
-                ds["puv_" + k].attrs["standard_name"] = standard_name[k]
-
-        # add in Hs
-        ds["puv_Hsp"] = np.sqrt(2) * ds["puv_Hrmsp"]
-        ds["puv_Hsp"].attrs["description"] = "Hs computed via sqrt(2) * Hrmsp"
-        ds["puv_Hsp"].attrs["standard_name"] = "sea_surface_wave_significant_height"
-        ds["puv_Hsp"].attrs["units"] = "m"
-
-        ds["puv_Hsu"] = np.sqrt(2) * ds["puv_Hrmsu"]
-        ds["puv_Hsu"].attrs["description"] = "Hs computed via sqrt(2) * Hrmsu"
-        ds["puv_Hsu"].attrs["standard_name"] = "sea_surface_wave_significant_height"
-        ds["puv_Hsu"].attrs["units"] = "m"
-
-        ds["puv_Hsp_tail"] = np.sqrt(2) * ds["puv_Hrmsp_tail"]
-        ds["puv_Hsp_tail"].attrs[
-            "description"
-        ] = "Hs computed via sqrt(2) * Hrmsp with tail applied"
-        ds["puv_Hsp_tail"].attrs[
-            "standard_name"
-        ] = "sea_surface_wave_significant_height"
-        ds["puv_Hsp_tail"].attrs["units"] = "m"
-
-        ds["puv_Hsu_tail"] = np.sqrt(2) * ds["puv_Hrmsu_tail"]
-        ds["puv_Hsu_tail"].attrs[
-            "description"
-        ] = "Hs computed via sqrt(2) * Hrmsu with tail applied"
-        ds["puv_Hsu_tail"].attrs[
-            "standard_name"
-        ] = "sea_surface_wave_significant_height"
-        ds["puv_Hsu_tail"].attrs["units"] = "m"
-
-        ds["puv_Tpp"].attrs["units"] = "s"
-
-        ds["puv_Tpu"].attrs["units"] = "s"
-
-        ds["puv_azr"].attrs["units"] = "degrees"
-        ds["puv_azr_tail"].attrs["units"] = "degrees"
+        ds = ds_puv(ds)
 
     # ds = utils.create_water_depth(ds)
 
@@ -230,5 +86,168 @@ def nc_to_waves(nc_filename):
     utils.check_compliance(nc_filename, conventions=ds.attrs["Conventions"])
 
     print("Done creating", nc_filename)
+
+    return ds
+
+
+def do_puv():
+    if ds.attrs["orientation"] != "UP":
+        raise NotImplementedError("PUV currently only works on UP oriented instruments")
+
+    print("Running puv_quick")
+
+    N, M = np.shape(ds["u_1205"].squeeze())
+
+    if "puv_first_frequency_cutoff" in ds.attrs:
+        first_frequency_cutoff = ds.attrs["puv_first_frequency_cutoff"]
+    else:
+        first_frequency_cutoff = 1 / 10
+
+    if "puv_last_frequency_cutoff" in ds.attrs:
+        last_frequency_cutoff = ds.attrs["puv_last_frequency_cutoff"]
+    else:
+        last_frequency_cutoff = 1 / 2.5
+
+    desc = {
+        "Hrmsp": f"Hrms (=Hmo) from pressure in freq. band {first_frequency_cutoff} <= f <= {last_frequency_cutoff}",
+        "Hrmsu": f"Hrms from u,v in freq. band {first_frequency_cutoff} <= f <= {last_frequency_cutoff}",
+        "ubr": f"Representative orbital velocity amplitude in freq. band {first_frequency_cutoff} <= f <= {last_frequency_cutoff}",
+        "omegar": f"Representative orbital velocity (radian frequency) in freq. band {first_frequency_cutoff} <= f <= {last_frequency_cutoff}",
+        "Tr": f"Representative orbital velocity period in freq. band {first_frequency_cutoff} <= f <= {last_frequency_cutoff}",
+        "Tpp": f"Peak period from pressure in freq. band {first_frequency_cutoff} <= f <= {last_frequency_cutoff}",
+        "Tpu": f"Peak period from velocity in freq. band {first_frequency_cutoff} <= f <= {last_frequency_cutoff}",
+        "phir": "Representative orbital velocity direction (angles from x-axis, positive ccw)",
+        "azr": "Representative orb. velocity direction (deg; geographic azimuth; ambiguous =/- 180 degrees)",
+        "ublo": f"ubr in freq. band f <= {first_frequency_cutoff}",
+        "ubhi": f"ubr in freq. band f >= {last_frequency_cutoff}",
+        "ubig": f"ubr in infra-gravity freq. band {first_frequency_cutoff} f <= 1/20",
+        "Hrmsp_tail": "Hrms (=Hmo) from pressure with f^-4 tail applied",
+        "Hrmsu_tail": "Hrms from u,v with f^-4 tail applied",
+        "phir_tail": "Representative orbital velocity direction (angles from x-axis, positive ccw) with f^-4 tail applied",
+        "azr_tail": "Representative orb. velocity direction (deg; geographic azimuth; ambiguous =/- 180 degrees) with f^-4 tail applied",
+        "Snp": f"Pressure-derived non-directional wave energy spectrum in freq. band {first_frequency_cutoff} <= f <= {last_frequency_cutoff}",
+        "Snp_tail": "Pressure-derived non-directional wave energy spectrum with f^-4 tail applied",
+        "frequencies": "Frequency",
+        "fclip": "Frequency",
+    }
+    standard_name = {
+        "Tpp": "sea_surface_wave_period_at_variance_spectral_density_maximum",
+        "Tpu": "sea_surface_wave_period_at_variance_spectral_density_maximum",
+        "phir": "sea_surface_wave_from_direction",
+        "phir_tail": "sea_surface_wave_from_direction",
+        "azr": "sea_surface_wave_from_direction",
+        "azr_tail": "sea_surface_wave_from_direction",
+        "Snp": "sea_surface_wave_variance_spectral_density",
+        "Snp_tail": "sea_surface_wave_variance_spectral_density",
+    }
+    unit = {
+        "Hrmsp": "m",
+        "Hrmsu": "m",
+        "ubr": "m s-1",
+        "omegar": "rad s-1",
+        "Tr": "s",
+        "Tpp": "s",
+        "Tpu": "s",
+        # "phir": "Representative orbital velocity direction (angles from x-axis, positive ccw)",
+        "azr": "degrees",
+        "ublo": "m s-1",
+        "ubhi": "m s-1",
+        "ubig": "m s-1",
+        "Hrmsp_tail": "m",
+        "Hrmsu_tail": "m",
+        # "phir_tail": "Representative orbital velocity direction (angles from x-axis, positive ccw) with f^-4 tail applied",
+        # "azr_tail": "Representative orb. velocity direction (deg; geographic azimuth; ambiguous =/- 180 degrees) with f^-4 tail applied",
+        # "Snp": "Pressure-derived non-directional wave energy spectrum",
+        # "Snp_tail": "Pressure-derived non-directional wave energy spectrum with f^-4 tail applied",
+    }
+
+    # puvs = {k: np.full_like(ds["time"].values, np.nan, dtype=float) for k in desc}
+    puvs = {k: [] for k in desc}
+
+    if "P_1ac" in ds:
+        pvar = "P_1ac"
+    else:
+        pvar = "P_1"
+
+    for n in tqdm(range(N)):
+        puv = waves.puv_quick(
+            ds[pvar][n, :].values,
+            ds["u_1205"][n, :],
+            ds["v_1206"][n, :],
+            ds[pvar][n, :].mean().values + ds.attrs["pressure_sensor_height"],
+            ds.attrs["pressure_sensor_height"],
+            ds.attrs["velocity_sample_volume_height"],
+            1 / ds.attrs["sample_interval"],
+            first_frequency_cutoff=first_frequency_cutoff,
+            last_frequency_cutoff=last_frequency_cutoff,
+        )
+
+        for k in puvs:
+            # puvs[k][n] = puv[k]
+            puvs[k].append(puv[k])
+
+    for k in puvs:
+        puvs[k] = np.array(puvs[k])
+
+    ds["puv_frequency"] = xr.DataArray(
+        puvs["frequencies"][0],
+        dims="puv_frequency",
+        attrs={"standard_name": "sea_surface_wave_frequency", "units": "Hz"},
+    )
+    ds["puv_frequency_clipped"] = xr.DataArray(
+        puvs["fclip"][0],
+        dims="puv_frequency_clipped",
+        attrs={"standard_name": "sea_surface_wave_frequency", "units": "Hz"},
+    )
+
+    for k in puvs:
+        if k == "frequencies" or k == "fclip":
+            continue
+        if puvs[k].ndim == 1:
+            ds["puv_" + k] = xr.DataArray(puvs[k], dims="time")
+        elif puvs[k].ndim == 2:
+            try:
+                ds["puv_" + k] = xr.DataArray(puvs[k], dims=["time", "puv_frequency"])
+            except ValueError:
+                ds["puv_" + k] = xr.DataArray(
+                    puvs[k], dims=["time", "puv_frequency_clipped"]
+                )
+        if k in desc:
+            ds["puv_" + k].attrs["description"] = desc[k]
+        if k in standard_name:
+            ds["puv_" + k].attrs["standard_name"] = standard_name[k]
+        if k in unit:
+            ds["puv_" + k].attrs["units"] = unit[k]
+
+    ds["puv_Hsp"] = np.sqrt(2) * ds["puv_Hrmsp"]
+    ds["puv_Hsp"].attrs["description"] = "Hs computed via sqrt(2) * Hrmsp"
+    ds["puv_Hsp"].attrs["standard_name"] = "sea_surface_wave_significant_height"
+    ds["puv_Hsp"].attrs["units"] = "m"
+
+    ds["puv_Hsu"] = np.sqrt(2) * ds["puv_Hrmsu"]
+    ds["puv_Hsu"].attrs["description"] = "Hs computed via sqrt(2) * Hrmsu"
+    ds["puv_Hsu"].attrs["standard_name"] = "sea_surface_wave_significant_height"
+    ds["puv_Hsu"].attrs["units"] = "m"
+
+    ds["puv_Hsp_tail"] = np.sqrt(2) * ds["puv_Hrmsp_tail"]
+    ds["puv_Hsp_tail"].attrs[
+        "description"
+    ] = "Hs computed via sqrt(2) * Hrmsp with f^-4 tail applied"
+    ds["puv_Hsp_tail"].attrs["standard_name"] = "sea_surface_wave_significant_height"
+    ds["puv_Hsp_tail"].attrs["units"] = "m"
+
+    ds["puv_Hsu_tail"] = np.sqrt(2) * ds["puv_Hrmsu_tail"]
+    ds["puv_Hsu_tail"].attrs[
+        "description"
+    ] = "Hs computed via sqrt(2) * Hrmsu with f^-4 tail applied"
+    ds["puv_Hsu_tail"].attrs["standard_name"] = "sea_surface_wave_significant_height"
+    ds["puv_Hsu_tail"].attrs["units"] = "m"
+
+    ds["puv_Tpp"].attrs["units"] = "s"
+
+    ds["puv_Tpu"].attrs["units"] = "s"
+
+    ds["puv_azr"].attrs["units"] = "degrees"
+    ds["puv_azr_tail"].attrs["units"] = "degrees"
 
     return ds
