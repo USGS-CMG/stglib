86cc80f54c56d6e2448472f2e03075e4c6754d8a
rbales-usgs <rbales@usgs.gov>
2024-05-28 12:57:12 -0400

    formatting

--- stglib/tests/test_scripts.py
+++ stglib/tests/test_scripts.py
@@ -194,6 +194,8 @@ def test_vec():
     vec_raw("gatts_NBM22CSB.txt", "config_NBM22CSB.yaml")
     vec_nc("NBMCSBvec01-raw.cdf")
     vec_wvs("NBMCSBvec01b-cal.nc")
+    vec_raw("glob_att1126_msl.txt", "config_1126vec14823.yaml")
+    vec_nc("1126vec14823-raw.cdf")
 
 
 def wxt_raw(glob_att, config_yaml):
