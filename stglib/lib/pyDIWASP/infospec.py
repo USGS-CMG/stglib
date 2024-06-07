84c915815a5040a5133992d13c0e66e8db2743e3
Dan Nowacki <dnowacki@usgs.gov>
2024-06-07 11:59:56 -0700

    update imports on pyDIWASP

--- stglib/lib/pyDIWASP/infospec.py
+++ stglib/lib/pyDIWASP/infospec.py
@@ -1,26 +1,26 @@
 import numpy as np
-from private.hsig import hsig
+from .private.hsig import hsig
 
 def infospec(SM):
     """
     DIWASP V1.4 function
     infospec: calculates and displays information about a directional spectrum
-    
+
     [Hsig,Tp,DTp,Dp]=infospec(SM)
-    
+
     Outputs:
     Hsig		Signficant wave height
     Tp			Peak period
     DTp		Direction of spectral peak
     Dp			Dominant direction
-    
+
     Inputs:
     SM   		A spectral matrix structure containing the file data
-    
-    Hsig is the significant wave height. Tp is the peak frequency, the highest point in the one dimensional spectrum. 
-    DTp is the main direction of the peak period (i.e the highest point in the two-dimensional directional spectrum). 
+
+    Hsig is the significant wave height. Tp is the peak frequency, the highest point in the one dimensional spectrum.
+    DTp is the main direction of the peak period (i.e the highest point in the two-dimensional directional spectrum).
     Dp is the dominant direction defined as the direction with the highest energy integrated over all frequencies.
-    
+
     "help data_structures" for information on the DIWASP data structures
 
     Copyright (C) 2002 Coastal Oceanography Group, CWR, UWA, Perth
@@ -40,11 +40,11 @@ def infospec(SM):
     print('Infospec::')
     print('Significant wave height: {}'.format(H))
     print('Peak period: {}'.format(Tp))
-    print('Direction of peak period: {} axis angle / {} ' 
+    print('Direction of peak period: {} axis angle / {} '
         'compass bearing'.format(DTp, compangle(DTp, SM['xaxisdir'])))
-    print('Dominant direction: {} axis angle / {} ' 
+    print('Dominant direction: {} axis angle / {} '
         'compass bearing'.format(Dp, compangle(Dp, SM['xaxisdir'])))
-    
+
     return H, Tp, DTp, Dp
 
 def compangle(dirs, xaxisdir):
--- stglib/lib/pyDIWASP/plotspec.py
+++ stglib/lib/pyDIWASP/plotspec.py
@@ -1,30 +1,30 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from private.spectobasis import spectobasis
+from .private.spectobasis import spectobasis
 
 def plotspec(SM, ptype):
     """
     DIWASP V1.4 function
     plotspec: plots the spectral matrix in 3D or polar form
-    
+
     plotspec(SM,ptype)
-    
+
     Inputs:
      SM   		A spectral matrix structure
      ptype		plot type:
        1	3D surface plot
-       2	polar type plot 
+       2	polar type plot
        3	3D surface plot (compass bearing angles direction from)
        4	polar type plot (compass bearing angles direction from)
-    
-    The 3D surface plot type is a MATLAB surface plot with SM.freqs on the x axis, SM.dirs on the y axis and the spectral density, SM.S as the z value. 
-    The polar type plot is a MATLAB polar plot with the direction showing values in SM.dirs, the radius showing values in SM.freqs 
+
+    The 3D surface plot type is a MATLAB surface plot with SM.freqs on the x axis, SM.dirs on the y axis and the spectral density, SM.S as the z value.
+    The polar type plot is a MATLAB polar plot with the direction showing values in SM.dirs, the radius showing values in SM.freqs
     and contours representing the spectral density, SM.S. An example of the polar type plot is shown on the front cover of the manual.
-    For plot types 1 and 2, the direction is the direction of propagation relative to the Cartesian axis. 
-    For options 3 and 4 the direction is coming from as a true compass bearing (this has changed from previous versions). 
+    For plot types 1 and 2, the direction is the direction of propagation relative to the Cartesian axis.
+    For options 3 and 4 the direction is coming from as a true compass bearing (this has changed from previous versions).
     Directions are corrected internally from the SM.xaxisdir and SM.dunit
-    fields that define the orientation of the axes and directional units in the spectral matrix. 
-    
+    fields that define the orientation of the axes and directional units in the spectral matrix.
+
     "help data_structures" for information on the DIWASP data structures
 
     Copyright (C) 2002 Coastal Oceanography Group, CWR, UWA, Perth
@@ -44,10 +44,10 @@ def plotspec(SM, ptype):
         else:
             xaxisdir = 90
         dirs = dirs + np.pi + np.pi * (90 - xaxisdir) / 180
-    
+
     #Surface plots
     if ptype == 1 or ptype == 3:
-        if ptype == 3: 
+        if ptype == 3:
             dirs %= 2 * np.pi
         order = np.argsort(dirs)
         dirs = (180 * dirs / np.pi)[order]
