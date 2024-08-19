d4115a41d21af72090977ad313f55e5c2a173418
Dan Nowacki <dnowacki@usgs.gov>
2024-08-19 14:23:46 -0700

    Deal with situation when K_p all <= 0.1

--- stglib/core/waves.py
+++ stglib/core/waves.py
@@ -181,7 +181,10 @@ def define_cutoff(f, Pxx, Kp, noise=0.9):
 
     fpeakcut = 1.1 * f[np.argmax(Pxx)]
     fpeakcutind = np.searchsorted(f, fpeakcut)  # cutoff based on 1.1*fp
-    Kpcutind = np.nonzero(Kp > 0.1)[0][-1] + 1  # cutoff keeping only Kp > 0.1
+    try:
+        Kpcutind = np.nonzero(Kp > 0.1)[0][-1] + 1  # cutoff keeping only Kp > 0.1
+    except IndexError:
+        Kpcutind = 0
 
     # take the more conservative of either K_p or noise cutoff
     if (noisecutind > fpeakcutind) and (noisecutind <= Kpcutind):
