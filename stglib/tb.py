8bb1062471b59939d09c8a9ec4947bea8b27d1cb
De Meo <odemeo@usg.gov>
2024-11-18 11:11:57 -0500

    Fixed typo

--- stglib/tb.py
+++ stglib/tb.py
@@ -198,7 +198,7 @@ def ds_add_attrs(ds):
             }
         )
         if "P_1ac_note" in ds.attrs:
-            ds["P_1ac"].attrs.update({"note": ds.attrs["P_1ac"]})
+            ds["P_1ac"].attrs.update({"note": ds.attrs["P_1ac_note"]})
 
     return ds
 
